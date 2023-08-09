9df4ea3944d9e6df7ac7933eefb23ef530fab898
sola <opop0628@gmail.com>
2023-08-09 18:22:43 +0900

    .

---
+++ projects/mmdet3d_plugin/core/bbox/transformer.py
@@ -0,0 +1,27 @@
+import torch
+
+def bboxmtv2result(bboxes, visibles, scores, labels):
+    """Convert detection results to a list of numpy arrays.
+
+    Args:
+        bboxes (torch.Tensor): Bounding boxes with shape of (n, 5).
+        labels (torch.Tensor): Labels with shape of (n, ).
+        scores (torch.Tensor): Scores with shape of (n, ).
+        attrs (torch.Tensor, optional): Attributes with shape of (n, ). \
+            Defaults to None.
+
+    Returns:
+        dict[str, torch.Tensor]: Bounding box results in cpu mode.
+
+            - boxes_3d (torch.Tensor): 3D boxes.
+            - scores (torch.Tensor): Prediction scores.
+            - labels_3d (torch.Tensor): Box labels.
+            - attrs_3d (torch.Tensor, optional): Box attributes.
+    """
+    result_dict = dict(
+        boxes_mtv2d=bboxes.to('cpu'),
+        visibles_mtv2d=visibles.cpu(),
+        scores_mtv2d=scores.cpu(),
+        labels_mtv2d=labels.cpu())
+
+    return result_dict
---
+++ projects/mmdet3d_plugin/models/detectors/tmvdet.py
@@ -0,0 +1,42 @@
+# ------------------------------------------------------------------------
+# Copyright (c) 2022 Toyota Research Institute, Dian Chen. All Rights Reserved.
+# ------------------------------------------------------------------------
+# Copyright (c) 2022 megvii-model. All Rights Reserved.
+# ------------------------------------------------------------------------
+# Modified from DETR3D (https://github.com/WangYueFt/detr3d)
+# Copyright (c) 2021 Wang, Yue
+# ------------------------------------------------------------------------
+# Modified from mmdetection3d (https://github.com/open-mmlab/mmdetection3d)
+# Copyright (c) OpenMMLab. All rights reserved.
+# ------------------------------------------------------------------------
+
+import torch
+import torch.nn.functional as F
+from einops import rearrange
+from mmcv.runner import force_fp32, auto_fp16
+from mmdet.models import DETECTORS
+#from mmdet3d.core import bbox3d2result
+from projects.mmdet3d_plugin.core.bbox.transformer import bboxmtv2result
+from mmdet3d.models.detectors.mvx_two_stage import MVXTwoStageDetector
+from projects.mmdet3d_plugin.models.utils.grid_mask import GridMask
+
+from .vedet import VEDet
+
+@DETECTORS.register_module()
+class TMVDet(VEDet):
+    def simple_test_pts(self, x, img_metas, gt_map=None, rescale=False):
+        """Test function of point cloud branch."""
+        outs = self.pts_bbox_head(x, img_metas)
+        results = dict()
+        if outs.get('all_cls_scores', None) is not None:
+            bbox_list = self.pts_bbox_head.get_bboxes(outs, img_metas, rescale=rescale)
+            #bbox_results = [bbox3d2result(bboxes, scores, labels) for bboxes, scores, labels in bbox_list]
+            bbox_results = [bboxmtv2result(bboxes, visibles, scores, labels) for bboxes, visibles, scores, labels in bbox_list]
+            results['bbox_results'] = bbox_results
+
+        if gt_map is not None:
+            seg_results = self.compute_seg_iou(outs)
+            results['seg_results'] = seg_results
+
+        return results
+
